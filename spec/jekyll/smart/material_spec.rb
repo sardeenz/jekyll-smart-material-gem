51bb03a3781e98569f2e7c7ebf62a9bebbc298bd
Chad Foley <chad.foley@raleighnc.gov>
2017-03-21 07:04:08 -0400

    first

---
+++ spec/jekyll/smart/material_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Jekyll::Smart::Material do
+  it 'has a version number' do
+    expect(Jekyll::Smart::Material::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
